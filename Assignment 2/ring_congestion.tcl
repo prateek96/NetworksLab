f97afc920d65906f080bce3b6e1052751633b392
Prateek Prince Thakur <prateekthakur1@gmail.com>
2018-02-10 03:52:40 +0530

    Add files via upload
    Assignment 2

---
+++ Assignment 2/ring_congestion.tcl
@@ -0,0 +1,60 @@
+set ns [new Simulator]
+$ns color 1 Red
+$ns color 2 Green
+
+set nf [open out.nam w]
+$ns namtrace-all $nf
+
+proc finish {} {
+	global ns nf
+	$ns flush-trace
+	close $nf
+	exec nam out &
+	exit 0
+}
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+
+$ns duplex-link $n0 $n1 2Mb 10ms DropTail
+$ns duplex-link-op $n0 $n1 queuePos 0.5
+$ns queue-limit $n0 $n1 2
+$ns duplex-link $n1 $n2 2Mb 10ms DropTail
+$ns duplex-link $n2 $n3 2Mb 10ms DropTail
+$ns duplex-link $n3 $n4 2Mb 10ms DropTail
+$ns duplex-link $n4 $n5 2Mb 10ms DropTail
+$ns duplex-link $n5 $n0 2Mb 10ms DropTail
+
+set tcp [new Agent/TCP]
+$tcp set class_ 2
+$ns attach-agent $n0 $tcp
+set sink [new Agent/TCPSink]
+$ns attach-agent $n1 $sink
+$ns connect $tcp $sink
+$tcp set fid_ 1
+
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+
+set tcp2 [new Agent/TCP]
+$tcp2 set class_ 2
+$ns attach-agent $n1 $tcp2
+set sink2 [new Agent/TCPSink]
+$ns attach-agent $n4 $sink2
+$ns connect $tcp2 $sink2
+$tcp2 set fid_ 2
+
+set ftp2 [new Application/FTP]
+$ftp2 attach-agent $tcp2
+
+$ns at 0.1 "$ftp start"
+$ns at 5.0 "$ftp stop"
+$ns at 0.4 "$ftp2 start"
+$ns at 3.0 "$ftp2 stop"
+$ns at 5.0 "finish"
+
+$ns run
